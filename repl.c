bc8bf3b79f1f8f6c19f0e0a358f45497f9e0e768
Kenny Rachuonyo <krmboya@gmail.com>
2015-09-02 17:15:51 +0300

    make it Win compatible

--- repl.c
+++ repl.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* if compiling for Windows, compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {};
+
+/* if not Windows, include the editline headers */
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
 
 int main(int arg, char** argv) {
 
@@ -22,9 +44,6 @@ int main(int arg, char** argv) {
 
     // free retrieved input
     free(input);
-
   }
-
   return 0;
-
 }
