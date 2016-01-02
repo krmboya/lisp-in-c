b66579763af58b10f149e63fc87b4c8e423ca1bf
Kenny Rachuonyo <krmboya@gmail.com>
2016-01-02 15:13:21 +0300

    simple evaluation of arithmetic

--- evaluation.c
+++ evaluation.c
@@ -27,6 +27,37 @@ void add_history(char* unused) {};
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+      
+long eval(mpc_ast_t* t) {
+
+  // if tagged as number, return it directly
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  // the operator is always the second child
+  char* op = t->children[1]->contents;
+
+  // store third child in `x`
+  long x = eval(t->children[2]);
+
+  // iterate remaining children combining
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int arg, char** argv) {
 
   /* Create parsers */
@@ -56,8 +87,9 @@ int main(int arg, char** argv) {
     // Attempt to parse the input
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      // print AST on success
-      mpc_ast_print(r.output);
+      // evaluate input and print result
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       // print error
