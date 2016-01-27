04d357b6048666b06a7c5d399464df99cdac0d1a
Kenny Rachuonyo <krmboya@gmail.com>
2016-01-27 12:13:43 +0300

    added lval reading functions

--- evaluation.c
+++ evaluation.c
@@ -109,6 +109,50 @@ void lval_del(lval* v) {
   free(v);
 }
 
+// return lval number (or error)
+lval* lval_read_num(mpc_ast* t) {
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ? lval_num(x) : lval_err("invalid number");
+}
+
+lval* lval_read(mpc_ast* t) {
+
+  // if symbol or number, return conversion to that type
+  if (strstr(t->tag, "number")) {
+    return lval_read_num(t);
+  }
+  
+  if (strstr(t->tag, "symbol")) {
+    return lval_sym(t->contents);
+  }
+
+  // if root (>) or sexpr, create empty list
+  lval* x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+  // fill the created list with any valid expressions contained within
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") ==  0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") ==  0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") ==  0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") ==  0) { continue; }
+    if (strcmp(t->children[i]->tag,  "regex") ==  0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+
+  return x;
+  
+}
+
+lval* lval_add(lval* v, lval* x) {
+  // Adds a new element to sexpr pointed by `v'
+  v->count++;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  v->cell[v->count-1] = x;
+  return v;
+}
 
 // print an lval
 void lval_print(lval v) {
